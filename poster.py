77e3a615f7f4b4dd0e75ca4c839291e9246b79a0
ranechka <ranechka@gmail.com>
2021-01-20 22:36:52 -0600

    Started Selenium interactions. Finished login.

--- poster.py
+++ poster.py
@@ -1,23 +1,61 @@
-import gspread
+import gspread, sys, json, time 
 from oauth2client.service_account import ServiceAccountCredentials
+from selenium import webdriver
 # from pprint import pprint
+
+f = open("details.json")
+details = json.load(f)
+f.close()
 
 row = int(input("Please input row of oldest unseen confession:: ")) # 6830
 lc = int(input("Please input the next LC value:: "))
 scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
 creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
 client = gspread.authorize(creds)
-# print("creds done")
-spreadsheet = client.open("Copy of UT Confessions (new) (Responses)") # Open the spreadhseet
-# print("Spreadsheet opened")
+spreadsheet = client.open(details["spreadsheet_name"]) # Open the spreadhseet
 sheet = spreadsheet.get_worksheet(0)
+
+
+# Selenium
+# Open a Chrome window
+chrome_options = webdriver.ChromeOptions()
+prefs = {"profile.default_content_setting_values.notifications" : 2}
+chrome_options.add_experimental_option("prefs",prefs)
+driver = webdriver.Chrome(options=chrome_options)
+
+# Log in to Facebook
+email = details["email"]
+password = details["password"]
+driver.get('https://www.facebook.com/')
+user =  driver.find_element_by_id('email')
+user.send_keys(email)
+pwd = driver.find_element_by_id('pass')
+pwd.send_keys(password)
+button = driver.find_element_by_id('u_0_b')
+button.submit()
+time.sleep(5)
+
+# Navigate to group
+group = details["group_link"]
+driver.get(group)
+time.sleep(3)
+
+# Change to page user
+driver.find_element_by_xpath("//div[@aria-label='Actor Selector']").click()
+time.sleep(2)
+paths = driver.find_elements_by_xpath('//span[contains(text(), "Bevo\'s Tea")]')
+
+userSelect = driver.find_element_by_xpath('//span[contains(text(), "Choose How to Interact")]//following::span[contains(text(), "Bevo\'s Tea")]')
+userSelect.click()
+#paths[-1].click() 
+
 
 # Gets the first unseen confession
 def getConfession():
     global row
     assert(row > 1)
     text = sheet.cell(row, 2).value # Get row's confession text
-    prevText = sheet.cell(row-1, 2).value # Get previous row's confession text
+    prevText = sheet.cell(row - 1, 2).value # Get previous row's confession text
     while(text == prevText): # Check for duplicate of the previous row
         row = row + 1
         prevText = text
@@ -44,8 +82,13 @@ def toPost(confession):
 # Formats the text into a confesison with LC number
 # Uses selenium to post to Facebook
 def fbPoster(text):
-    print("YOU MADE IT")
-    pass
+    lc_text = "#LC" + str(lc)
+    confess = '"{}"'.format(text)
+    final_message = lc_text + '\n' + confess
+    
+    # Post to facebook group
+    print(final_message)
+
 
 
 def teardown():
