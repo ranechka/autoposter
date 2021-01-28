ade50ec1164860062c19295f197d8e707706de61
ranechka <ranechka@gmail.com>
2021-01-27 22:49:54 -0600

    Functionality complete. Finished posting, finished get row and LC from spreadsheet, finished updating spreadsheet. Moved to final spreadsheet.

--- poster.py
+++ poster.py
@@ -1,20 +1,19 @@
 import gspread, sys, json, time 
 from oauth2client.service_account import ServiceAccountCredentials
 from selenium import webdriver
-# from pprint import pprint
 
 f = open("details.json")
 details = json.load(f)
 f.close()
 
-row = int(input("Please input row of oldest unseen confession:: ")) # 6830
-lc = int(input("Please input the next LC value:: "))
 scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
 creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
 client = gspread.authorize(creds)
 spreadsheet = client.open(details["spreadsheet_name"]) # Open the spreadhseet
 sheet = spreadsheet.get_worksheet(0)
 
+row = len(sheet.col_values(4)) + 1 # Column 4 is Seen?
+lc = int(sheet.col_values(5)[-1]) + 1 # Column 5 is LC#### if posted
 
 # Selenium
 # Open a Chrome window
@@ -42,12 +41,11 @@ time.sleep(3)
 
 # Change to page user
 driver.find_element_by_xpath("//div[@aria-label='Actor Selector']").click()
-time.sleep(2)
-paths = driver.find_elements_by_xpath('//span[contains(text(), "Bevo\'s Tea")]')
+time.sleep(1)
 
 userSelect = driver.find_element_by_xpath('//span[contains(text(), "Choose How to Interact")]//following::span[contains(text(), "Bevo\'s Tea")]')
 userSelect.click()
-#paths[-1].click() 
+time.sleep(2)
 
 
 # Gets the first unseen confession
@@ -55,6 +53,9 @@ def getConfession():
     global row
     assert(row > 1)
     text = sheet.cell(row, 2).value # Get row's confession text
+    if not text:
+        print("All caught up!")
+        teardown()
     prevText = sheet.cell(row - 1, 2).value # Get previous row's confession text
     while(text == prevText): # Check for duplicate of the previous row
         row = row + 1
@@ -69,31 +70,41 @@ def toPost(confession):
     global row
     print(confession)
     answer = input("Post this confession? (yes/no) \nAnything else for exit\n")
-    if(answer == 'no'):
-        # Mark as seen
+    answer = answer.lower()
+    if(answer == 'no' or answer == 'n'):
+        sheet.update_cell(row, 4, "SEEN") # Mark as seen
         row = row + 1
-    elif(answer == 'yes'):
-        # Mark as seen
-        row = row + 1
+        print("\n**************************************************\n")
+    elif(answer == 'yes' or answer == 'y'):
+        sheet.update_cell(row, 4, "SEEN") # Mark as seen
         fbPoster(confession)
+        print("\n**************************************************\n")
     else:
         teardown()
 
 # Formats the text into a confesison with LC number
 # Uses selenium to post to Facebook
 def fbPoster(text):
-    lc_text = "#LC" + str(lc)
+    global row, lc
+    lcText = "#LC" + str(lc)
     confess = '"{}"'.format(text)
-    final_message = lc_text + '\n' + confess
+    finalMessage = lcText + '\n' + confess
     
     # Post to facebook group
-    print(final_message)
-
-
+    postBox = driver.find_element_by_xpath('//span[contains(text(), "What\'s on your mind?")]')
+    postBox.click()
+    time.sleep(2)
+    driver.switch_to.active_element.send_keys(finalMessage)
+    driver.find_element_by_xpath("//div[@aria-label='Post']").click()
+    sheet.update_cell(row, 5, lc) # Mark as posted
+    row = row + 1
+    lc = lc + 1
+    print("Posted:")
 
 def teardown():
-      # Close selenium
-      sys.exit("Later bitch")
+    driver.quit()
+    sys.exit("Later bitch")
 
-toPost(getConfession())
+while(True):
+    toPost(getConfession())
 
